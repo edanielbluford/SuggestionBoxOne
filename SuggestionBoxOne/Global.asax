3641e72eb2b55253588b13e6f55e6ff134934ab5
edanielbluford <e.daniel.bluford@gmail.com>
2018-07-10 14:46:35 -0400

    first commit

---
+++ SuggestionBoxOne/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SuggestionBoxOne.MvcApplication" Language="C#" %>
---
+++ SuggestionBoxOne/Models/SuggestionModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SuggestionBoxOne.Models
+{
+    public class SuggestionModel
+
+    {
+        [Key]
+        public int RecordNum { get; set; }
+
+
+        public string SuggestionName { get; set; }
+        public String SuggestionComment { get; set; }
+    }
+}
